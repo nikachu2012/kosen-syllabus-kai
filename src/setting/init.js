6dba03d4e98656ea5f4c1833841df68e4fc2daf7
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2023-03-22 02:27:35 +0900

    [add] 設定を追加

---
+++ src/setting/init.js
@@ -0,0 +1,4 @@
+window.onload = () => {
+    document.querySelector('#kai_nonLoad_content').classList.add('hidden')
+    document.querySelector('#kai_loaded_content').classList.remove('hidden')
+}
--- src/teacher/LoadToJSON.js
+++ src/teacher/LoadToJSON.js
@@ -183,4 +183,5 @@ const LoadToJSON = (data, loaded) => {
     document.querySelector("#sidebar_teacher").href = '/teacher/?loaded=' + encodeURIComponent(loaded);
     document.querySelector('#sidebar_officialSyllabus').href = data.url;
     document.querySelector('#sidebar_browserStorage').href = '/browserStorage/?loaded=' + encodeURIComponent(loaded);
+    document.querySelector('#sidebar_setting').href = '/setting/?loaded=' + encodeURIComponent(loaded);
 }
