98346ce31194cef74de598147597506bc3473a43
Nikachu <75615396+nikachu2012@users.noreply.github.com>
2023-03-27 01:55:26 +0900

    [fix] 初期読み込み時にLocalStorageにデータ生成

--- src/LoadSetting.js
+++ src/LoadSetting.js
@@ -5,4 +5,13 @@ let Setting;
         Setting = JSON.parse(localStorage.getItem('setting'));
         document.documentElement.setAttribute("data-theme", Setting.appearance.selectedColor);
     }
+    else {
+        const setData = {
+            appearance: {
+                unacceptableIsRed: false,
+                selectedColor: "black",
+            }
+        }
+        localStorage.setItem('setting', JSON.stringify(setData))
+    }
 })();
